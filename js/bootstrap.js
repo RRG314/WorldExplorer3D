de535da4bc16d7aa6c10fb3392e19ab0cbc7f125
Steven Reid <stevenreid@Macmini.lan>
2026-02-13 19:36:01 -0500

    fix: harden title selector clicks and bump cache bust to v16

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4,8 +4,8 @@ import {
   moduleEntrypoint,
   vendorScriptsCritical,
   vendorScriptsOptional
-} from './modules/manifest.js?v=15';
-import { loadScriptList } from './modules/script-loader.js?v=15';
+} from './modules/manifest.js?v=16';
+import { loadScriptList } from './modules/script-loader.js?v=16';
 
 async function boot() {
   try {
