639a620a154eb30f861eda22956919aea0cb06d4
Steven Reid <sreid1118@gmail.com>
2026-07-27 08:43:52 -0400

    fix: use terrain source contract at runtime

--- app/js/bootstrap.js
+++ app/js/bootstrap.js
@@ -4,7 +4,7 @@ import {
   moduleEntrypoint,
   vendorScriptsCritical,
   vendorScriptsOptional
-} from './modules/manifest.js?v=286';
+} from './modules/manifest.js?v=287';
 import { loadScriptList } from './modules/script-loader.js?v=56';
 import {
   initStartupDiagnostics,
