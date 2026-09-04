34801662ad8dc78cba583971cf416c702c803032
Steven Reid <sreid1118@gmail.com>
2026-09-03 22:20:23 -0400

    fix: keep interior entrances clear of solid walls

--- app/js/interiors.js
+++ app/js/interiors.js
@@ -1,5 +1,5 @@
 import { ctx as appCtx } from "./shared-context.js?v=55";
-import { buildInteriorScene, canPublishInteriorConnector } from "./interiors/scene-builder.js?v=12";
+import { buildInteriorScene, canPublishInteriorConnector } from "./interiors/scene-builder.js?v=13";
 import {
   INTERIOR_ENTRY_RADIUS,
   INTERIOR_INTERACTION_MOVE_EPSILON,
